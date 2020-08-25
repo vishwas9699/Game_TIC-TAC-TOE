980b56cf887442012bed1b0f4bd6b6954278c90e
vishwas9699 <vvishwas179@gmail.com>
2020-08-25 19:59:43 +0530

    ic

--- tictactoe.py
+++ tictactoe.py
@@ -4,11 +4,25 @@ pygame.init()
 
 WIDTH=600
 HEIGHT=600
+LINE_WIDTH=15
 RED=(255,0,0)
+BACKGROUND_COLOUR=(28,170,156)
+LINE_COLOR=(23,145,135)
 
 screen=pygame.display.set_mode( (WIDTH,HEIGHT) )
 pygame.display.set_caption('TIC TAC TOE')
-screen.fill(RED)
+screen.fill(BACKGROUND_COLOUR)
+
+
+#pygame.draw.line(screen,RED,(10,10),(300,300),10)
+
+def draw_lines():
+    pygame.draw.line(screen,LINE_COLOR,(0,200),(600,200),LINE_WIDTH)
+    pygame.draw.line(screen,LINE_COLOR,(0,400),(600,400),LINE_WIDTH)
+    pygame.draw.line(screen,LINE_COLOR,(200,0),(200,600),LINE_WIDTH)
+    pygame.draw.line(screen,LINE_COLOR,(400,0),(400,600),LINE_WIDTH)
+
+draw_lines()
 
 #mainloop
 while True:
