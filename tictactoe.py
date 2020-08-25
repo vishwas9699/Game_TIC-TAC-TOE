2c196a8bba3d4f2686aec3b227469a086bf66626
vishwas9699 <vvishwas179@gmail.com>
2020-08-25 21:00:26 +0530

    ic

--- tictactoe.py
+++ tictactoe.py
@@ -8,7 +8,13 @@ HEIGHT=600
 LINE_WIDTH=15
 BOARD_ROWS=3
 BOARD_COLOUMN=3
+CIRCLE_RADIUS=60
+CIRCLE_WIDTH=15
+CROSS_WIDTH=25
+SPACE=55
 RED=(255,0,0)
+CREAM=(239,231,200)
+BLACK=(0,0,0)
 BACKGROUND_COLOUR=(28,170,156)
 LINE_COLOR=(23,145,135)
 
@@ -23,6 +29,15 @@ def draw_lines():
     pygame.draw.line(screen,LINE_COLOR,(0,400),(600,400),LINE_WIDTH)
     pygame.draw.line(screen,LINE_COLOR,(200,0),(200,600),LINE_WIDTH)
     pygame.draw.line(screen,LINE_COLOR,(400,0),(400,600),LINE_WIDTH)
+
+def darw_figures():
+    for row in range(BOARD_ROWS):
+        for coloumn in range(BOARD_COLOUMN):
+            if board[row][coloumn]==1:
+                pygame.draw.circle(screen,CREAM,(int(coloumn*200+100),int(row*200+100)),CIRCLE_RADIUS,CIRCLE_WIDTH )
+            elif board[row][coloumn]==2:
+                pygame.draw.line(screen,BLACK,(coloumn*200+SPACE,row*200+200-SPACE),(coloumn*200+200-SPACE,row*200+SPACE),CROSS_WIDTH)
+
 
 def mark_square(row,coloumn,player):
     board[row][coloumn]=player
@@ -65,7 +80,9 @@ while True:
                 elif player ==2:
                     mark_square(clicked_row,clicked_coloumn,2)
                     player =1
-                print(board)
+
+                darw_figures()
+                
 
 
 
