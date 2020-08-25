c9de8063ddcbe077217929febf222cd5bcaa743e
vishwas9699 <vvishwas179@gmail.com>
2020-08-25 19:37:05 +0530

    ic

--- tictactoe.py
+++ tictactoe.py
@@ -4,13 +4,16 @@ pygame.init()
 
 WIDTH=600
 HEIGHT=600
+RED=(255,0,0)
 
 screen=pygame.display.set_mode( (WIDTH,HEIGHT) )
 pygame.display.set_caption('TIC TAC TOE')
-
+screen.fill(RED)
 
 #mainloop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    pygame.display.update()        
